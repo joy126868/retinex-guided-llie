e6529e0e58ece0c9e26ed3cee53e67eadfec8cf4
joy126868 <51686155+joy126868@users.noreply.github.com>
2026-01-19 18:37:47 +0800

    Update train.py

--- train.py
+++ train.py
@@ -210,5 +210,5 @@ def train_pipeline(root_path):
 
 
 if __name__ == '__main__':
-    root_path = r"D:/CUE-master"
+    root_path = osp.abspath(osp.join(__file__, osp.pardir))
     train_pipeline(root_path)
